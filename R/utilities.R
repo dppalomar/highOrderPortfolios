5285fe6a532d18e2445e7da708c200b70efd3d94
xwangew <xwangew@ecelvd370.ece.local>
2022-10-05 20:14:16 +0800

    add estimate_skew_t

--- R/utilities.R
+++ R/utilities.R
@@ -1,3 +1,87 @@
+#' @title Estimate the parameters of skew-t distribution from multivariate observations
+#'
+#' @description Using the package fitHeavyTail to estimate the parameters of ghMST distribution from multivariate observations, namely,
+#' location vector (mu), skewness vector (gamma), scatter matrix (scatter), degree of freedom (nu), parameters a,
+#' and the Cholesky decomposition of the scatter matrix (chol_Sigma).
+#'
+#' @author Xiwen Wang, Rui Zhou and Daniel P. Palomar
+#'
+#' @references
+#' Aas, Kjersti and Ingrid Hobæk Haff. "The generalized hyperbolic skew student’st-distribution,"
+#' Journal of financial econometrics, pp. 275-309, 2006.
+#' 
+#' @param X Data matrix containing the multivariate time series (each column is one time series).
+#' @param initial List of initial values of the parameters for the iterative estimation method.
+#'                Possible elements include:
+#'                \itemize{\item{\code{nu}: default is \code{4},}
+#'                         \item{\code{mu}: default is the data sample mean,}
+#'                         \item{\code{gamma}: default is the sample skewness vector,}
+#'                         \item{\code{scatter}: default follows from the scaled sample covariance matrix,}
+#'                         }
+#' @param nu_lb Minimum value for the degree of freedom to maintain the existence of high-order moments (default is \code{9}).
+#' @param max_iter Integer indicating the maximum number of iterations for the iterative estimation
+#'                 method (default is \code{100}).
+#' @param ptol Positive number indicating the relative tolerance for the change of the variables
+#'             to determine convergence of the iterative method (default is \code{1e-3}).
+#' @param ftol Positive number indicating the relative tolerance for the change of the log-likelihood
+#'             value to determine convergence of the iterative method (default is \code{Inf}, so it is
+#'             not active). Note that using this argument might have a computational cost as a convergence
+#'             criterion due to the computation of the log-likelihood (especially when \code{X} is high-dimensional).
+#' @param PXEM Logical value indicating whether to use the parameter expansion (PX) EM method to accelerating the convergence.
+#' @param return_iterates Logical value indicating whether to record the values of the parameters (and possibly the
+#'                        log-likelihood if \code{ftol < Inf}) at each iteration (default is \code{FALSE}).
+#' @param verbose Logical value indicating whether to allow the function to print messages (default is \code{FALSE}).
+#'
+#' @return A list containing the following elements:
+#'         \item{\code{mu}}{Location vector estimate (not the mean).}
+#'         \item{\code{gamma}}{Skewness vector estimate.}
+#'         \item{\code{scatter}}{Scatter matrix estimate.}
+#'         \item{\code{nu}}{Degrees of freedom estimate.}
+#'         \item{\code{chol_Sigma}}{Choleski decomposition of the Scatter matrix estimate.}
+#'         \item{\code{a}}{A list of coefficients useful for later computation}
+#'
+#' @examples
+#' library(highOrderPortfolios)
+#' data("X50")
+#' X_skew_t_params <- estimate_skew_t(X50)
+#'
+#' @import fitHeavyTail
+#' @export
+estimate_skew_t <- function(X, initial = NULL, nu_lb = 9, max_iter = 100, ptol = 1e-3, ftol = Inf,
+                            PXEM = TRUE, return_iterates = FALSE, verbose = FALSE) { 
+  options(nu_min = nu_lb)
+  X_skew_t_params <- fitHeavyTail::fit_mvst(X, max_iter = max_iter, ptol = ptol, ftol = ftol,
+                                            PXEM = PXEM, return_iterates = return_iterates, verbose = verbose)
+  return_paramters <- list()
+  return_paramters$mu    <- X_skew_t_params$mu
+  return_paramters$nu    <- X_skew_t_params$nu
+  return_paramters$gamma <- X_skew_t_params$gamma
+  return_paramters$scatter <- X_skew_t_params$scatter
+  return_paramters$chol_Sigma <- chol(return_paramters$scatter)
+  
+  ## Compute a given paramters of skew-t model
+  compute_a <- function(X_skew_t_params) {
+    a11 <- (X_skew_t_params$nu)/(X_skew_t_params$nu-2)
+    a21 <- (X_skew_t_params$nu)/(X_skew_t_params$nu-2)
+    a22 <- (2 * X_skew_t_params$nu ** 2)/((X_skew_t_params$nu - 2) ** 2 * (X_skew_t_params$nu - 4))
+    a31 <- 16 * (X_skew_t_params$nu ** 3)/(((X_skew_t_params$nu - 2) ** 3) * (X_skew_t_params$nu - 4) * (X_skew_t_params$nu - 6))
+    a32 <- 6 * (X_skew_t_params$nu ** 2)/(((X_skew_t_params$nu -2) ** 2) * (X_skew_t_params$nu - 4))
+    a41 <- (12 * X_skew_t_params$nu + 120) * (X_skew_t_params$nu ** 4)/ (((X_skew_t_params$nu - 2) ** 4) * (X_skew_t_params$nu - 4) * (X_skew_t_params$nu - 6) * (X_skew_t_params$nu - 8))
+    a42 <- (2 * X_skew_t_params$nu + 4) * (X_skew_t_params$nu ** 3)/(((X_skew_t_params$nu - 2) ** 3) * (X_skew_t_params$nu - 4) * (X_skew_t_params$nu - 6)) * 6
+    a43 <- (X_skew_t_params$nu ** 2)/((X_skew_t_params$nu - 2) * (X_skew_t_params$nu - 4)) * 3
+    return(list("a11" = a11, "a21" = a21, "a22" = a22,
+                "a31" = a31, "a32" = a32,
+                "a41" = a41, "a42" = a42, "a43" = a43))
+  }
+  
+  return_paramters$a <- compute_a(return_paramters)
+  attr(return_paramters, "type") <- "X_skew_t_params"
+  return(return_paramters)
+}
+
+
+
+
 #' @title Estimate first four moment parameters of multivariate observations
 #'
 #' @description Estimate first four moments of multivariate observations, namely,
@@ -81,6 +165,9 @@ estimate_sample_moments <- function(X, adjust_magnitude = FALSE) {
 
 
 
+
+
+
 #' @title Evaluate first four moments of a given portfolio
 #'
 #' @description Evaluate first four moments of a given portfolio's return, namely,
@@ -129,10 +216,17 @@ eval_portfolio_moments <- function(w, X_statistics) {
              skewness = as.numeric(portm3(w = w, M3 = X_statistics$Phi)),
              kurtosis = as.numeric(portm4(w = w, M4 = X_statistics$Psi))))
   } else if (attr(X_statistics, "type") == "X_skew_t_params") {
-    # Xiwen
+    wT_mu <- t(w) %*% X_statistics$mu
+    wT_gamma <- t(w) %*% X_statistics$gamma
+    wT_Sigma_w <- sum((X_statistics$chol_Sigma %*% w) ** 2)
+    return(c(mean     = as.numeric(wT_mu + X_statistics$a$a11 * wT_gamma),
+             variance = as.numeric(X_statistics$a$a21 * wT_Sigma_w  + X_statistics$a$a22 * ((wT_gamma)**2) ),
+             skewness = as.numeric(X_statistics$a$a31 * ((wT_gamma) ** 3) + X_statistics$a$a32 * (wT_gamma) * ( wT_Sigma_w) ),
+             kurtosis = as.numeric(X_statistics$a$a41 * ((wT_gamma) ** 4) + X_statistics$a$a42 * (wT_Sigma_w) * ((wT_gamma) ** 2) + X_statistics$a$a43 * ((wT_Sigma_w) ** 2))))
   } else
     stop("Unknown type of argument ", dQuote(X_statistics), " : it should be returned from either", dQuote("estimate_sample_moments()"), 
     " or ", dQuote("estimate_skew_t()"))
 }
+
 
 
