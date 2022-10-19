7ee9a833155c29b706badf364f21162136f70c03
dppalomar <daniel.p.palomar@gmail.com>
2022-10-19 11:22:41 +0800

    Version updated.

--- R_buildignore/developer_commands.R
+++ R_buildignore/developer_commands.R
@@ -40,6 +40,6 @@ devtools::build()
 #devtools::revdep(pkg = "highOrderPortfolios")  # to check reverse dependencies
 #devtools::check_win_release()  #to check under windows
 #R CMD build . --resave-data  # this is to generate tarball
-#R CMD check highOrderPortfolios_0.0.2.tar.gz --as-cran --run-donttest  # this is before submission to CRAN
-#R CMD install highOrderPortfolios_0.0.2.tar.gz
+#R CMD check highOrderPortfolios_0.1.0.tar.gz --as-cran --run-donttest  # this is before submission to CRAN
+#R CMD install highOrderPortfolios_0.1.0.tar.gz
 #submit the tarball directly via the webform: https://cran.r-project.org/submit.html
